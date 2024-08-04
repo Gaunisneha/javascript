a1c2c55af3f920fdb18e74df8d8658eeec8d8bbc
Gaunisneha <snehagauni@gmail.com>
2024-08-04 16:11:53 +0000

    string values

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -2,6 +2,14 @@ let value=3
 let negvalue=-value
 console.log(negvalue);
 
+let str1="hello sneha";
+let str2=" gauni";
 
+let str3=str1+str2;
+console.log(str3);
 
+console.log("1"+2);
 
+console.log(1+2+"2");
+
+// node 01_basics/variables.js
